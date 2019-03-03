6a09ff99c6a86209103ecaeff25cfa16da7570fb
b4ry <barry90@interia.pl>
2019-03-03 22:55:30 +1000

    - implemented logic to determine size of ExplorationGameAreaPanel

---
+++ Irregular2Dmap/Assets/Scripts/Controllers/Panels/RegionUIPanels/ExplorationPanel/ExplorationGameAreaPanelController.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Scripts.Models;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers.Panels.RegionUIPanels.ExplorationPanel
+{
+    public class ExplorationGameAreaPanelController : MonoBehaviour
+    {
+        private const int TileSideSize = 36;
+        private const int TileOffset = 2;
+        private const int TileXPadding = 20;
+        private const int TileYPadding = 10;
+
+        public GameObject ExplorationGamePanel;
+        public GameObject ExplorationGameAreaPanel;
+
+        private SortedList<int, int> ranges = new SortedList<int, int>();
+
+        void Awake()
+        {
+            for(int i = 10, j = 2; i <= 120; i += 10, j++)
+            {
+                ranges.Add(i, j);
+            }
+        }
+
+        void Start()
+        {
+            var regionModel = SelectedRegionsController.Instance.SelectedRegionObjects.FirstOrDefault().RegionModel;
+
+            SetupPanel(regionModel.Biomes[0]);
+        }
+
+        public void SetupPanel(BiomeModel biomeModel)
+        {
+            var tilesNumber = ranges.FirstOrDefault(r => r.Key >= biomeModel.Area).Value;
+
+            var width = (tilesNumber * TileSideSize + (tilesNumber - 1) * TileOffset + TileXPadding);
+            var height = (tilesNumber * TileSideSize + (tilesNumber - 1) * TileOffset + TileYPadding);
+
+            ExplorationGameAreaPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        }
+    }
+}
